9a65b57176db7c84d10818dc66d0286653f8d410
Valentin <johannsen@pflegonaut.de>
2019-08-07 18:45:17 +0200

    dynamische Listen

--- understanding-the-base-features/src/App.js
+++ understanding-the-base-features/src/App.js
@@ -11,8 +11,9 @@ class App extends Component {
       { name: 'Valentin', age: 25 },
       { name: 'Samuel', age: 26 },
       { name: 'Finja', age: 25 }
-      
-    ]
+    ],
+    otherState : 'some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -45,6 +46,11 @@ class App extends Component {
     )
   }
 
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   randomizeNumber = () => {
      const min = 1;
      const max = 100;
@@ -58,34 +64,106 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      margin: '10px'
     };
+
+  {/* Listen anzeigen Java Script Weg*/}
+  let persons = null;
+  
+  if (this.state.showPersons) {
+    // einzeln anzeigen
+      // persons = (
+      //   <Person />
+      // );
+
+    // dynamische Listen
+      persons = (
+        <div>
+          {this.state.persons.map(person => {
+            return <Person 
+                      name={person.name} 
+                      age={person.age} />
+          })}
+        </div>
+
+      )
+  }
 
     return (
       <div className="App">
+
+      {/* Start */}
         <h1>Hi Ich bin eine React App</h1>
+
+      {/* Inline Styling, Btn Handling Weg 1 */}
         <button 
           // inline styling
           style={buttonStyle}
 
+          // btn handling Weg 1
           onClick={
-          //Weg 1
           () => this.switchNameHandler('Creck!')}>switch name</button>
+
+        {/* Ausgabe eines states */}
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age} />
+
+        {/* Klick auf Text, Btn handling Weg 2   */}
         <Person
+          // Ausgabe
           name='Samuel'
           age='25'
           clickAsAReference={
             //Weg 2 empfohlen
             this.switchNameHandler.bind(this, 'Bachmann!')}>Mein Hobby ist surfen!</Person>
+
+        {/* Output/state live aendern bei Input-Eingabe  */}
         <Person 
           changed={this.nameChangeHandler}
           name={this.state.persons[3].name}
         />
+
+        {/* einfachste Ausgabe */}
         <Person
-          name='Frank' age='63' />           
+          name='Frank' age='63' />   
+
+        {/* Listen anzeigen */}
+        <button 
+          // Button
+          style={buttonStyle}
+          onClick={this.togglePersonHandler}>show persons</button>
+          
+          {this.state.showPersons === true ?
+          // bei true-> Anzeigen der Liste
+            <div>
+              <Person
+                name={this.state.persons[2].name}
+                age={this.state.persons[2].age} />
+              <Person
+                name='Samuel'
+                age='25'
+                clickAsAReference={
+                  this.switchNameHandler.bind(this, 'Bachmann!')}>Mein Hobby ist surfen!</Person>
+              <Person 
+                changed={this.nameChangeHandler}
+                name={this.state.persons[3].name}
+              />
+              <Person
+                name='Frank' age='63' />   
+            </div> 
+
+          // bei false-> null
+          : null}
+
+          {/* Listen anzeigen Java Script Weg*/}
+          <button style={buttonStyle}
+          onClick={this.togglePersonHandler}>show persons2</button>
+        
+          {persons}
+
+          {/* dynamische Listen/ Arrays anzeigen */}
       </div>
     );
   }
